d8a77bf4f60e7aa6620a3cb8334f0a8675315050
stevenkeezer <stevengkeezer@gmail.com>
2020-04-09 21:31:57 -0700

    added require.env

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Geosuggest from "react-geosuggest";
 import IosLocateOutline from "react-ionicons/lib/IosLocateOutline";
 
 import "./App.css";
+require("dotenv").config();
 
 function App() {
   const [Weather, setWeather] = useState(null);
@@ -29,7 +30,7 @@ function App() {
   };
 
   const getDataApi = async (lat, long) => {
-    const API_KEY = "008eb2e476bb4ac1ce396f0211fba6fc";
+    const API_KEY = process.env.REACT_APP_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${API_KEY}`;
 
     try {
@@ -45,7 +46,7 @@ function App() {
   };
 
   const getFiveDayApi = async (lat, long) => {
-    const API_KEY = "008eb2e476bb4ac1ce396f0211fba6fc";
+    const API_KEY = process.env.REACT_APP_KEY;
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=imperial&src/img/sunset.svg&appid=${API_KEY}`;
 
     try {
